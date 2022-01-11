e2773b7c1fdbea185aebb3e24cf2b4f97c31073d
b4lanc3 <tranbinhminh615@gmail.com>
2022-01-11 17:06:58 +0700

    a

--- projectSilver/loginpage/app.js
+++ projectSilver/loginpage/app.js
@@ -13,23 +13,3 @@ $(".log-in").click(function(){
   });
 var name = document.getElementById('name');
 var pw = document.getElementById('pw');
-
-function store() {
-    localStorage.setItem('name', name.value);
-    localStorage.setItem('pw', pw.value);
-}
-
-function check() {
-
-    var storedName = localStorage.getItem('name');
-    var storedPw = localStorage.getItem('pw');
-
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-
-    if(userName.value !== storedName || userPw.value !== storedPw) {
-        alert('ERROR');
-    }else {
-        alert('You are loged in.');
-    }
-}
