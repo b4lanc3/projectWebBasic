43d05001965529ee6f11c4d8f7ebef877ba2126d
b4lanc3 <tranbinhminh615@gmail.com>
2022-03-03 19:31:49 +0700

    a

--- JSI/JSI-SS03/app.js
+++ JSI/JSI-SS03/app.js
@@ -1,4 +1,35 @@
 //Bài 1
-let arr = [1, 2, 3, 4]
-let copy = [...arr]
+//cách 1
+const arr1 = [1, 2, 3, 5]
+const copy = [...arr]
 console.log(copy);
+//cách 2
+const arr1 = [1, 2, 3, 5]
+const copy = arr.slice();
+console.log(copy);
+//Bài 2
+//cách 1
+const arrA = ["Hello", "Xin chào"]
+const arrB = ["Bonjour ", "Olá "]
+const waysToSayHello = [...arrA, ...arrB]
+console.log(waysToSayHello);
+//cách 2
+const arrA = ["Hello", "Xin chào"]
+const arrB = ["Bonjour ", "Olá "]
+const waysToSayHello = arrA.concat(arrB);
+//Bài 3
+//arrSquare
+const arrX = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const arrSquare = arrX.map(x => x * x);
+console.log(arrSquare);
+//arrOdds
+const arrX = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const arrOdds = arrX.filter(x => x % 2 != 0);
+console.log(arrOdds);
+//Bài 4
+function filterRange(arr, a, b) {
+    return arr.filter(item => (a <= item && item <= b));
+}
+const arr = [1, 5, 4, 6];
+const filtered = filterRange(arr, 0, 2);
+console.log(filtered);
